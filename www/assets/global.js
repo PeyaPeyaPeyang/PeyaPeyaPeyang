3e4fa6abc12d6d48b9b490c3932ed50f0b8d11b0
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-08-06 07:35:14 +0900

    広告をつくった

--- www/assets/global.js
+++ www/assets/global.js
@@ -1,14 +1,51 @@
 const headerLoadListeners = [];
+const closeKohkokuListeners = [];
 
 window.onHeaderLoad = (func) => {
     headerLoadListeners.push(func);
 };
+
+window.onAdClose = (func) => {
+    closeKohkokuListeners.push(func);
+}
+
+const kohkokus = [
+    [
+        "Lorem_ipsum_cloud", "https://cloud.lorem.ipsum/"
+    ]
+];
+
+const kohkokuHTML = (name, url) => `
+    <div id="kohkoku">
+        <a href="${url}" target="_blank"><img style="cursor: pointer" id="kohkoku-image" src="/assets/kohkokus/${name}.svg"></a>
+        <span class="button" onclick="closeKohkoku()"><a href="#">➕</a></span>
+        <span id="kohkoku-information">この広告は1分以上更新がない場合に表示されます。このページの更新/GitHub経由での更新後、24時間以内に表示されなくなるはずです。</span>
+    </div>
+`;
+
+const genKohkokuElement = () => {
+    const kohkokuFooter = document.createElement('footer');
+    const kohkoku = kohkokus[Math.floor(Math.random() * kohkokus.length)];
+
+    kohkokuFooter.innerHTML = kohkokuHTML(kohkoku[0], kohkoku[1]);
+    return kohkokuFooter;
+}
+
+
+const closeKohkoku = () => {
+    document.getElementById('kohkoku').style = 'display: none';
+    closeKohkokuListeners.forEach(func => func());
+};
+
 
 window.addEventListener("load", () => {
     const header = "/assets/header.htm";
 
     const header_container = document.getElementsByTagName("header");
     const header_container_0 = header_container[0];
+
+    const body = document.getElementsByTagName("body")[0];
+    body.appendChild(genKohkokuElement());
 
     fetch(header)
         .then(response => response.text())
@@ -32,6 +69,7 @@ window.addEventListener("load", () => {
         .catch(error => {
             console.log(error);
         })
+
 })
 
 document.oncontextmenu = () => {
